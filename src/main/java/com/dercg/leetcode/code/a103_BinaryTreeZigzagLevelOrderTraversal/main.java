84939193b8854f4dee9f259fdaa95a4e729103d9
DerCgs <mail@changlu.me>
2020-09-22 18:23:29 +0800

    add

--- src/main/java/com/dercg/leetcode/code/a103_BinaryTreeZigzagLevelOrderTraversal/main.java
+++ src/main/java/com/dercg/leetcode/code/a103_BinaryTreeZigzagLevelOrderTraversal/main.java
@@ -33,6 +33,46 @@ public class main {
     }
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> results = new ArrayList<>();
+
+        LinkedList<TreeNode> node_queue = new LinkedList<>();
+        node_queue.addLast(root);
+        node_queue.addLast(null);
+
+        LinkedList<Integer> level_list = new LinkedList<>();
+        boolean is_order_left = true;
+
+        while (node_queue.size() > 0) {
+            TreeNode curr_node = node_queue.pollFirst();
+            if (curr_node != null) {
+                if (is_order_left)
+                    level_list.addLast(curr_node.val);
+                else
+                    level_list.addFirst(curr_node.val);
+
+                if (curr_node.left != null)
+                    node_queue.addLast(curr_node.left);
+                if (curr_node.right != null)
+                    node_queue.addLast(curr_node.right);
+
+            } else {
+                // we finish the scan of one level
+                results.add(level_list);
+                level_list = new LinkedList<>();
+                // prepare for the next level
+                if (node_queue.size() > 0)
+                    node_queue.addLast(null);
+                is_order_left = !is_order_left;
+            }
+        }
+        return results;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder_inorder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
         Queue<TreeNode> q = new LinkedList<>();
